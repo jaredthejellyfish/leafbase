629143e7abde3e64e0c96f166a41e234bf4309b0
Gerard Almenara <ger.almenara@gmail.com>
2023-06-13 03:29:05 +0200

    --

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-"use client";
-
-import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
-import { toast } from "react-toastify";
+import React from "react";
 
 export default function Home() {
-  const session = useSession();
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-16">
-
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-16"></main>
   );
 }
--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect } from "react";
 
 const useLocalStorage = (key: string, initialValue: string) => {

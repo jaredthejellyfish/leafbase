be347d0bdf9f56b49bf4d8af2e99c9f6c21dacf3
Gerard Almenara <ger.almenara@gmail.com>
2023-06-25 17:42:13 +0200

    removed csp

--- next.config.js
+++ next.config.js
@@ -1,29 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' va.vercel-scripts.com;
-  child-src example.com;
-  style-src 'self' 'unsafe-inline';
-  font-src 'self';
-  connect-src vitals.vercel-insights.com;
-`;
-
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
-          },
-        ],
-      },
-    ];
-  },
   images: {
     domains: [
       "images.birdfact.com",

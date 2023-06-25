a95184c27259245f4d00150088ef1b0c34b6191c
Gerard Almenara <ger.almenara@gmail.com>
2023-06-25 12:16:37 +0200

    --

--- app/strains/page.tsx
+++ app/strains/page.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 };
 
 const Strains = async (props: Props) => {
-  const { strains, count, error } = await useServerStrains(1, 9);
+  const { strains, count, error } = await useServerStrains(1, 12);
   if (error) {
     return <div>Error!</div>;
   }

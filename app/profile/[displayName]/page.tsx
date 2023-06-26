4e88ecb803941118a5267608d676932209ed4022
Gerard Almenara <ger.almenara@gmail.com>
2023-06-27 01:24:28 +0200

    --

--- app/profile/[displayName]/page.tsx
+++ app/profile/[displayName]/page.tsx
@@ -207,12 +207,20 @@ const ProfileDisplay = async (props: Props) => {
         <div id="vertical 2" className="flex flex-col gap-4 lg:w-2/3">
           <div className="flex flex-col w-full shadow-md p-7 rounded-xl dark:bg-zinc-900">
             <h1 className="text-xl font-bold">General information</h1>
-            <h2 className="mt-4 text-lg">About me</h2>
-            <p className="mt-1 text-sm text-zinc-400 lg:w-4/5">
-              {user?.aboutMe}
-            </p>
+            {user?.aboutMe && (
+              <>
+                <h2 className="mt-4 text-lg">About me</h2>
+                <p className="mt-1 text-sm text-zinc-400 lg:w-4/5">
+                  {user?.aboutMe}
+                </p>
+              </>
+            )}
 
-            <div className="flex flex-col justify-between mt-6 md:flex-row md:w-4/5">
+            <div
+              className={`flex flex-col justify-between ${
+                user?.aboutMe && "mt-6"
+              } md:flex-row md:w-4/5`}
+            >
               <span className="mt-3 text-sm dark:text-white">
                 Birthday:
                 <p className="text-gray-400 w-60">
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -175,12 +175,20 @@ async function UserProfile({}: Props) {
         <div id="vertical 2" className="flex flex-col gap-4 pb-3 lg:w-2/3">
           <div className="flex flex-col w-full shadow-md p-7 rounded-xl dark:bg-zinc-900">
             <h1 className="text-xl font-bold">General information</h1>
-            <h2 className="mt-4 text-lg">About me</h2>
-            <p className="mt-1 text-sm text-zinc-400 lg:w-4/5">
-              {user?.aboutMe}
-            </p>
+            {user?.aboutMe && (
+              <>
+                <h2 className="mt-4 text-lg">About me</h2>
+                <p className="mt-1 text-sm text-zinc-400 lg:w-4/5">
+                  {user?.aboutMe}
+                </p>
+              </>
+            )}
 
-            <div className="flex flex-col justify-between mt-6 md:flex-row md:w-4/5">
+            <div
+              className={`flex flex-col justify-between ${
+                user?.aboutMe && "mt-6"
+              } md:flex-row md:w-4/5`}
+            >
               <span className="mt-3 text-sm dark:text-white">
                 Birthday:
                 <p className="text-gray-400 w-60">

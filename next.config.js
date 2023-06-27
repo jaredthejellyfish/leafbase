b2b198fbef0da9ab552197e57e678d035e6ef602
Gerard Almenara <ger.almenara@gmail.com>
2023-06-27 17:23:08 +0200

    --

--- next.config.js
+++ next.config.js
@@ -1,26 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' vitals.vercel-insights.com;
-  img-src 'self' companieslogo.com leafly-public.s3-us-west-2.amazonaws.com s3-us-west-2.amazonaws.com images.leafly.com avatars.githubusercontent.com www.gravatar.com developers.google.com discord.com lh3.googleusercontent.com;
-  connect-src 'self' vitals.vercel-insights.com;
-`;
-
-async function headers() {
-  return [
-    {
-      source: '/(.*)',
-      headers: [
-        {
-          key: 'Content-Security-Policy',
-          value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-        },
-      ],
-    },
-  ];
-}
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -36,7 +15,6 @@ const nextConfig = {
       "lh3.googleusercontent.com",
     ],
   },
-  headers,
 };
 
 module.exports = nextConfig;

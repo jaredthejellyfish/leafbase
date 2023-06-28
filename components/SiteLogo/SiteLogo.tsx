1156da763a681f8ec85885d2a27ec2a442052a96
Gerard Almenara <ger.almenara@gmail.com>
2023-06-28 19:48:38 +0200

    Added site logo and favicon

---
+++ components/SiteLogo/SiteLogo.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import siteLogo from "@/public/site-logo.webp";
+import Image from "next/image";
+
+type Props = { height?: number; width?: number };
+
+const SiteLogo = (props: Props) => {
+  return (
+    <Image
+      src={siteLogo}
+      alt="site logo"
+      className="border shadow rounded-xl border-zinc-100 dark:border-zinc-500"
+      height={props.height || 64}
+      width={props.width || 64}
+    />
+  );
+};
+
+export default SiteLogo;

e44735e450e669ef7ba7fe324853f45d129a64ba
Gerard Almenara <ger.almenara@gmail.com>
2023-06-30 00:28:12 +0200

    removed timer wrapper

--- leafbase_api/main.py
+++ leafbase_api/main.py
@@ -11,24 +11,15 @@ load_dotenv(dotenv_path="leafbase_api/.env")
 url: str = os.getenv("DB_URL")
 key: str = os.getenv("DB_API_KEY")
 
-vdb = VectorDB(url= url, key= key)
+vdb = VectorDB(url=url, key=key)
 app = FastAPI()
 
-def timer(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        print('{} ran in: {} seconds'.format(func.__name__, end - start))
-        return result
-    return wrapper
 
-@timer
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@timer
+
 @app.post("/liked-plot")
 async def liked_plot(strains: List[str]):
     return vdb.liked_strains_spread(strains)

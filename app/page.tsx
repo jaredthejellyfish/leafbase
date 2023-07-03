9aec4bc4d2a80d6b367f4fa5a9b542f66e9c10b6
Gerard Almenara <ger.almenara@gmail.com>
2023-07-03 19:06:35 -0400

    fixed build issue with stray modal in "/"

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,7 @@ import React from "react";
 export default async function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24 pt-16">
-      <Modal
-        title="Hello world!"
-        containerClasses={"xh-4/7 md:h-4/7 xl:w-2/5 md:w-2/3"}
-      >
-        <p>Hi</p>
-      </Modal>
+
     </main>
   );
 }

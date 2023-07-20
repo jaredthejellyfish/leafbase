d69358f513758fa5e9fd06c52844094f984739ed
Gerard Almenara <ger.almenara@gmail.com>
2023-07-20 10:59:56 +0200

    fixed padding in profile comment like button

--- app/profile/components/ProfileComments/StrainCommentLikeButton.tsx
+++ app/profile/components/ProfileComments/StrainCommentLikeButton.tsx
@@ -78,7 +78,7 @@ const StrainCommentLikeButton = (props: Props) => {
   return (
     <motion.button
       aria-label="Like Strain"
-      className={`z-40 absolute top-2 right-2.5 border bg-white dark:bg-zinc-800 text-zinc-400/75 transition-colors rounded-full p-1.5 dark:text-zinc-400 ${
+      className={`z-40 absolute top-1.5 right-2 border bg-white dark:bg-zinc-800 text-zinc-400/75 transition-colors rounded-full p-1.5 dark:text-zinc-400 ${
         liked ? 'border-green-600/40' : 'dark:border-zinc-700'
       }`}
       onClick={(e) => handleLike(e)}

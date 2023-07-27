605470e21ff4bab97ac10c1c0ee51efe198c0475
Gerard Almenara <ger.almenara@gmail.com>
2023-07-27 04:14:34 +0200

    -----

--- app/dispensaries/page.tsx
+++ app/dispensaries/page.tsx
@@ -7,7 +7,7 @@ import DispensariesMap from './components/DispensariesMap/DispensariesMap';
 import DispensariesMapSkeleton from './components/DispensariesMap/DispensariesMapSkeleton';
 import DispensariesListSkeleton from './components/DispensariesList/DispensariesListSkeleton';
 import DispensariesList from './components/DispensariesList/DispensariesList';
-import DispensariesMapError from './components/DispensariesMap/DispensariesMapError';
+import DispensariesMapError from './components/DispensariesMap/DispensariesMapError'
 
 export const metadata = {
   title: 'Dispensaries - Leafbase',
@@ -19,14 +19,14 @@ const Dispensaries = async () => {
   return (
     <div className="flex flex-col lg:flex-row justify-center w-full -mt-3 items-around">
       <div className="order-last lg:order-first">
-        <ErrorBoundary fallback={<DispensariesMapError />}>
+        <ErrorBoundary fallback={<DispensariesListSkeleton />}>
           <Suspense fallback={<DispensariesListSkeleton />}>
             <DispensariesList user={user && user} />
           </Suspense>
         </ErrorBoundary>
       </div>
       <div className="order-first lg:order-last">
-        <ErrorBoundary fallback={<DispensariesMapSkeleton />}>
+        <ErrorBoundary fallback={<DispensariesMapError />}>
           <DispensariesMap user={user && user} />
         </ErrorBoundary>
       </div>

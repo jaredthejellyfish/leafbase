99d4277260a784f679c2519c5cafa60e3f3e9bdd
Gerard Almenara <ger.almenara@gmail.com>
2023-08-02 05:24:20 +0200

    Added verification error to auth/error

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import { PiKeyReturnFill } from 'react-icons/pi';
 
 const AuthError = (context: { searchParams: { error: string } }) => {
-  console.log(context.searchParams.error);
   return (
     <div
       style={{ minHeight: 'calc(100vh - 145px)' }}
@@ -11,11 +10,14 @@ const AuthError = (context: { searchParams: { error: string } }) => {
     >
       <div className="flex flex-col items-center justify-center w-full gap-3 px-8 py-10 rounded-lg shadow-lg md:w-96 dark:bg-zinc-900">
         <h1 className="mb-1 text-2xl font-medium dark:text-white text-center">
-          {context.searchParams.error || 'There was an error'}
+          {context.searchParams.error === 'Verification'
+            ? 'Verification Error'
+            : context.searchParams.error || 'There was an error'}
         </h1>
         <p className="text-sm text-center text-zinc-400">
-          There was an error while trying to authenticate you. Please try again
-          later.
+          {context.searchParams.error === 'Verification'
+            ? 'There was an error while trying to verify your email address. Please try again later.'
+            : 'There was an error while trying to authenticate you. Please try again later.'}
         </p>
         <div className="flex flex-row gap-4"></div>
         <Link href="/auth/login" className="text-green-700">

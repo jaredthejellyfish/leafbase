f1ef0eda41f5753f0ba8a6d0c87737e31a3c63be
Gerard Almenara <ger.almenara@gmail.com>
2023-08-08 15:55:12 +0200

    updated errored icon

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { PiKeyReturnFill } from 'react-icons/pi';
+import { BsArrowReturnLeft } from 'react-icons/bs';
 import { ERRORS, DEFAULT_ERROR } from './errors';
 
 const generateError = (error?: string) => {
@@ -21,14 +21,14 @@ const AuthError = (context: { searchParams: { error: string } }) => {
       style={{ minHeight: 'calc(100vh - 145px)' }}
       className="flex items-center justify-center px-4 dark:bg-black"
     >
-      <div className="flex flex-col items-center justify-center w-full gap-3 px-8 py-10 rounded-lg shadow-lg md:w-96 dark:bg-zinc-900">
+      <div className="flex flex-col items-center justify-center w-full gap-3 px-8 pt-8 pb-6 rounded-lg shadow-lg md:w-96 dark:bg-zinc-900">
         <h1 className="mb-1 text-2xl font-medium dark:text-white text-center">
           {title}
         </h1>
         <p className="text-sm text-center text-zinc-400">{body}</p>
         <div className="flex flex-row gap-4"></div>
-        <Link href="/auth/login" className="text-green-700">
-          <PiKeyReturnFill size={40} />
+        <Link href="/auth/login" className="bg-green-700 rounded text-white p-2">
+          <BsArrowReturnLeft size={20} />
         </Link>
       </div>
     </div>

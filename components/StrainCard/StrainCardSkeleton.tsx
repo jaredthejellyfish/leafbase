35dce2621488e760376fe5fdd4d7d846b4d3213b
Gerard Almenara <ger.almenara@gmail.com>
2023-08-09 01:56:01 +0200

    Fixed loadie for StrainCard

--- components/StrainCard/StrainCardSkeleton.tsx
+++ components/StrainCard/StrainCardSkeleton.tsx
@@ -14,19 +14,19 @@ const StrainCardSkeleton = () => {
         ></div>
       </div>
       <div className="w-1/2 md:w-full">
-        <div className="w-14 inline-block px-2 py-1 text-xs font-medium bg-gray-200 rounded dark:shadow dark:bg-zinc-700">
-          <div className="w-full h-4 rounded-md"></div>
+        <div className="mb-0 w-14 h-6 inline-block px-2 text-xs font-medium bg-gray-200 rounded dark:shadow dark:bg-zinc-700">
+          <div className="w-full h-3 rounded-md"></div>
         </div>
-        <div className="px-1 mt-1 font-medium">
+        <div className="px-1 font-medium">
           <div className="w-full h-full rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
         </div>
         <div className="px-1 mt-1 font-medium">
           <div className="mb-2 w-2/5 h-4 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
         </div>
         <div className="px-1 text-sm font-normal text-gray-500 h-14 line-clamp-3">
-          <div className="mb-1.5 w-full h-3 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
-          <div className="mb-1.5 w-2/3 h-3 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
-          <div className="w-1/2 h-3 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
+          <div className="mb-1.5 w-full h-2.5 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
+          <div className="mb-1.5 w-2/3 h-2.5 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
+          <div className="w-1/2 h-2.5 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
         </div>
         <div className="flex flex-row items-center gap-1 p-1 mt-2 text-sm">
           <span className="flex items-center justify-center w-6 h-4">5.0</span>
@@ -42,18 +42,18 @@ const StrainCardSkeleton = () => {
           <span className="">CBD: 0%</span>
         </div>
 
-        <div className="flex flex-col px-1 mt-2 text-xs font-medium capitalize md:flex-row md:gap-3 md:items-center">
+        <div className="flex flex-col px-1 mt-2 text-xs gap-y-1 font-medium capitalize md:flex-row md:gap-3 md:items-center">
           <span className="flex flex-row items-center gap-1">
             <div
               className="rounded-full w-2.5 h-2.5 bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"
             ></div>
-            <div className="w-16 h-2.5 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
+            <div className="w-16 h-2 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
           </span>
           <span className="flex flex-row items-center gap-1">
             <div
               className="rounded-full w-2.5 h-2.5 bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"
             ></div>
-            <div className="w-16 h-2.5 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
+            <div className="w-16 h-2 rounded-md bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 animate-pulse"></div>
           </span>
         </div>
       </div>

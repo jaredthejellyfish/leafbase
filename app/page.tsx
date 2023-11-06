4eae70f0a9baee9fb6232b7d151dbe1dca6cc223
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-07 00:39:06 +0100

    Remove search functionality from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-'use client';
-
-import React, { useState } from 'react';
-
-import SearchResults from '@/components/SearchResults';
-import { Input } from '@/components/ui/input';
+import React from 'react';
 
 export default function Home() {
-  const [query, setQuery] = useState<string | undefined>(undefined);
-
-  return (
-    <main>
-      <Input value={query} onChange={(e) => setQuery(e.target.value)} />
-      <SearchResults query={query} />
-    </main>
-  );
+  return <main></main>;
 }

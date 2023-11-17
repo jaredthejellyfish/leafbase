eeb1764ea67480f34598b35a9f4babf568811e8d
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-17 23:05:09 +0100

    Refactor Modal component to use LazyMotion and update containerClass prop

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -3,13 +3,16 @@
 import React, { useEffect, useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import { useOnClickOutside } from 'usehooks-ts';
-import { motion } from 'framer-motion';
+import { LazyMotion, m } from 'framer-motion'; //changed motion to m and imported LazyMotion, domAnimation
+
+import { cn } from '@/lib/utils';
 
 type Props = {
   children: React.ReactNode;
   open: boolean;
   setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
   title: string;
+  containerClass?: string;
 };
 
 const variants = {
@@ -42,7 +45,7 @@ const variants = {
   },
 };
 
-function Modal({ children, open, setOpen, title }: Props) {
+function Modal({ children, open, setOpen, title, containerClass }: Props) {
   const [isOpen, setIsOpen] = useState(open);
   const ref = React.useRef<HTMLDivElement | null>(null);
 
@@ -78,28 +81,35 @@ function Modal({ children, open, setOpen, title }: Props) {
   }
 
   return (
-    <motion.div
-      variants={variants.background}
-      animate={isOpen ? 'visible' : 'hidden'}
-      initial="hidden"
-      className="fixed top-0 w-full h-screen z-50 px-6 bg-zinc-800/75 overflow-y-hidden flex items-center justify-center border-transparent right-0 left-0 bottom-0"
+    <LazyMotion
+      features={() => import('framer-motion').then((res) => res.domAnimation)}
     >
-      <motion.div
-        variants={variants.modal}
+      <m.div
+        variants={variants.background}
         animate={isOpen ? 'visible' : 'hidden'}
         initial="hidden"
-        ref={ref}
-        className="dark:bg-zinc-800 z-50 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full border-transparent relative"
+        className="fixed top-0 w-full h-screen z-50 px-6 bg-zinc-900/75 overflow-y-hidden flex items-center justify-center border-transparent right-0 left-0 bottom-0"
       >
-        <div className="w-full flex flex-row justify-between items-center dark:bg-zinc-950/40 bg-zinc-200 px-4 py-2">
-          <h2 className="text-lg font-semibold mt-0.5">{title}</h2>
-          <button onClick={() => handdleClose()}>
-            <AiOutlineClose size={20} />
-          </button>
-        </div>
-        <div className="px-4 py-2">{children}</div>
-      </motion.div>
-    </motion.div>
+        <m.div
+          variants={variants.modal}
+          animate={isOpen ? 'visible' : 'hidden'}
+          initial="hidden"
+          ref={ref}
+          className={cn(
+            'dark:bg-zinc-800 z-50 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full border-transparent relative',
+            containerClass || ''
+          )}
+        >
+          <div className="w-full flex flex-row justify-between items-center dark:bg-zinc-950/40 bg-zinc-200 px-4 py-2">
+            <h2 className="text-lg font-semibold mt-0.5">{title}</h2>
+            <button onClick={() => handdleClose()}>
+              <AiOutlineClose size={20} />
+            </button>
+          </div>
+          <div className="px-4 py-2">{children}</div>
+        </m.div>
+      </m.div>
+    </LazyMotion>
   );
 }
 

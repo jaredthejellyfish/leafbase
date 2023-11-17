20ea3d202d65a9f586baa7f2fc45c27347e6f2cb
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-17 03:19:39 +0100

    Add bundle analyzer to Next.js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
   reactStrictMode: true,
   devIndicators: {
@@ -26,4 +32,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);

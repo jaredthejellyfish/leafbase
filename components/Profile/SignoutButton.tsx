0f6d87ec6146cbbe65943fa64234436035f5c4f2
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-18 03:02:27 +0100

    Fix ariaLabel value in SignoutButton component

--- components/Profile/SignoutButton.tsx
+++ components/Profile/SignoutButton.tsx
@@ -9,7 +9,7 @@ const SignoutButton = () => {
       <SubmitButton
         text={'Sign out'}
         whilePending={'Signing out...'}
-        ariaLabel='Sign out'
+        ariaLabel="Sign out"
         className="w-full mt-5 text-white bg-green-700 hover:bg-green-800 transition-all focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-700 dark:hover:bg-green-800 focus:outline-none dark:focus:ring-blue-800"
       />
     </form>

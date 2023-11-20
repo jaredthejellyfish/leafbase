73bb4ba799d39f7fd18c78bca490f13687384e51
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-20 06:07:56 +0100

    Update hostname in next.config.js

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: '*.leafly.com',
+        hostname: 'euwnyenhzhztqztezjdn.supabase.co',
       },
       {
         protocol: 'https',

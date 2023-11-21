466eaccf3f763823cc53b1505689a44b10a3bbd3
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-21 16:23:15 +0100

    Refactor strains page layout

--- app/(strains)/strains/page.tsx
+++ app/(strains)/strains/page.tsx
@@ -44,27 +44,29 @@ async function StrainsPage(request: { searchParams: { filter?: string } }) {
 
   return (
     <main className="pb-8 px-5 md:px-16 xl:px-36 py-3">
-      <div className="">
-        <h1 className="mt-4 mb-2 text-3xl font-bold ">All strains</h1>
-        <p className="">
-          Browse the most comprehensive weed strain library on the web. Browse
-          weed strains by cannabis type (indica, sativa, or hybrid), effects, or
-          number of comments.
-        </p>
-        <div className="flex items-center justify-between px-1 font-medium">
-          <span className="mt-4 text-xs text-zinc-400">{count} strains</span>
-          <span className="flex flex-row items-center gap-1 mb-1 text-xs text-zinc-400">
-            <ErrorBoundary fallback={<FilterByMenuSkeleton />}>
-              <Suspense fallback={<FilterByMenuSkeleton />}>
-                <FilterByMenu filter={request.searchParams.filter} />
-              </Suspense>
-            </ErrorBoundary>
+      <div className="flex flex-col items-center">
+        <div id="heading" className="sm:max-w-[992px] xl:max-w-[1328px] w-full">
+          <h1 className="mt-4 mb-2 text-3xl font-bold ">All strains</h1>
+          <p className="">
+            Browse the most comprehensive weed strain library on the web. Browse
+            weed strains by cannabis type (indica, sativa, or hybrid), effects,
+            or number of comments.
+          </p>
+          <div className="flex items-center justify-between px-1 font-medium">
+            <span className="mt-4 text-xs text-zinc-400">{count} strains</span>
+            <span className="flex flex-row items-center gap-1 mb-1 text-xs text-zinc-400">
+              <ErrorBoundary fallback={<FilterByMenuSkeleton />}>
+                <Suspense fallback={<FilterByMenuSkeleton />}>
+                  <FilterByMenu filter={request.searchParams.filter} />
+                </Suspense>
+              </ErrorBoundary>
+            </span>
+          </div>
+          <span className="hidden w-full p-2 mt-1 text-xs border rounded md:block border-zinc-600/50 text-zinc-600">
+            These results are based on user comments and are not a substitute
+            for professional medical advice.
           </span>
         </div>
-        <span className="hidden w-full p-2 mt-1 text-xs border rounded md:block border-zinc-600/50 text-zinc-600">
-          These results are based on user comments and are not a substitute for
-          professional medical advice.
-        </span>
         <div className="flex flex-col gap-4 items-center justify-center w-full">
           <ErrorBoundary fallback={<div>Something went wrong</div>}>
             {strains && count && count > perPage && (

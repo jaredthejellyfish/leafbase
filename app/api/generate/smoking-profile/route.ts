287156bc15d9b77ac929ad70f52a61c30c187c9c
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-21 16:23:07 +0100

    Add smoking profile route

---
+++ app/api/generate/smoking-profile/route.ts
@@ -0,0 +1,131 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+import type { AggregatedData, StrainData } from '@/lib/types';
+import type { Database } from '@/lib/database';
+
+export async function GET(request: NextRequest) {
+  try {
+    const url = new URL(request.url);
+    const searchParams = new URLSearchParams(url.search);
+    const partial = searchParams.get('partial');
+
+    const supabase = createRouteHandlerClient<Database>({
+      cookies: () => cookies(),
+    });
+
+    const { data: sessionData, error: sessionError } =
+      await supabase.auth.getSession();
+
+    const { session } = sessionData;
+
+    if (sessionError || !session || !session.user) {
+      console.error(sessionError);
+      return NextResponse.json('Error getting session', { status: 400 });
+    }
+
+    const { data: likedStrains, error: likedStrainsError } = await supabase
+      .from('strain_likes')
+      .select('strain_id, strain_id:strains (effects, id)')
+      .eq('user_id', session.user.id);
+
+    if (likedStrainsError) {
+      console.error(likedStrainsError);
+      return NextResponse.json('Error getting liked strains', { status: 400 });
+    }
+
+    const likedStrainIds = likedStrains.map((strain) => {
+      return { id: strain.strain_id?.id };
+    });
+
+    const { data: strainEffects, error: strainEffectsError } = await supabase
+      .from('strains')
+      .select('thcPercent, effects, terps')
+      .in(
+        'id',
+        likedStrainIds.map((strain) => strain.id)
+      )
+      .returns<StrainData[]>();
+
+    if (strainEffectsError) {
+      console.error(strainEffectsError);
+      return NextResponse.json('Error getting strain effects', { status: 400 });
+    }
+
+    const aggregatedData: AggregatedData = {
+      averageThcPercent: 0,
+      effectScores: {},
+      terpeneScores: {},
+    };
+
+    let thcTotal = 0;
+    let thcCount = 0;
+
+    strainEffects.forEach((strain) => {
+      // Aggregate THC percent
+      if (strain.thcPercent !== null) {
+        thcTotal += strain.thcPercent;
+        thcCount++;
+      }
+
+      // Aggregate effects
+      Object.values(strain.effects).forEach((effect) => {
+        if (!aggregatedData.effectScores[effect.name]) {
+          aggregatedData.effectScores[effect.name] = 0;
+        }
+        aggregatedData.effectScores[effect.name] += effect.score;
+      });
+
+      // Aggregate terpenes
+      Object.values(strain.terps).forEach((terpene) => {
+        if (!aggregatedData.terpeneScores[terpene.name]) {
+          aggregatedData.terpeneScores[terpene.name] = 0;
+        }
+        aggregatedData.terpeneScores[terpene.name] += terpene.score;
+      });
+    });
+
+    // Calculate average THC percent
+    aggregatedData.averageThcPercent = thcTotal / thcCount;
+
+    const effects = Object.entries(aggregatedData.effectScores)
+      .sort(([, a], [, b]) => b - a)
+      .slice(0, partial === 'true' ? 4 : undefined)
+      .reduce((obj, [key, val]) => ({ ...obj, [key]: val }), {});
+
+    const terpenes = Object.entries(aggregatedData.terpeneScores)
+      .sort(([, a], [, b]) => b - a)
+      .slice(0, partial === 'true' ? 4 : undefined)
+      .reduce((obj, [key, val]) => ({ ...obj, [key]: val }), {});
+
+    // Merge the flattened effects and terpenes into a single object
+    const flattenedResponse = {
+      thc: aggregatedData.averageThcPercent,
+      ...effects,
+      ...terpenes,
+    };
+
+    // Find the maximum and minimum values in the flattenedResponse object
+    const values = Object.values(flattenedResponse);
+    const maxVal = Math.max(...values);
+    const minVal = Math.min(...values);
+
+    // Calculate the shift needed to make all values positive
+    const shift = minVal < 0 ? Math.abs(minVal) : 0;
+
+    // Normalize each value in the flattenedResponse object
+    const normalizedResponse: { [key: string]: number } = {};
+    for (const [key, val] of Object.entries(flattenedResponse)) {
+      normalizedResponse[key] = ((val + shift) / (maxVal + shift)) * 50;
+    }
+
+    return NextResponse.json(normalizedResponse, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json('Error generating smoking profile', {
+      status: 500,
+    });
+  }
+}

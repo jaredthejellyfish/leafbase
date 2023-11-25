c7df50e6fa65cce30b6890c8810e6828e509aef2
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-25 22:23:44 +0100

    Update ESLint configuration and ignore files to parse typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,29 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+
   extends: [
     'plugin:@typescript-eslint/recommended',
     'next',
     'next/core-web-vitals',
     'plugin:react/recommended',
     'plugin:@tanstack/eslint-plugin-query/recommended',
-    "plugin:tailwindcss/recommended"
+    'plugin:tailwindcss/recommended',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.eslint.json'],
+    tsconfigRootDir: __dirname,
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12, // Equivalent to ES2021, so might be updated to match the env.es2021.
+    ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
     'react/jsx-uses-vars': 'off',
     '@typescript-eslint/consistent-type-imports': 'error',
-    'tailwindcss/no-custom-classname': 'off'
+    'tailwindcss/no-custom-classname': 'off',
+    '@typescript-eslint/prefer-readonly': 'warn',
   },
 };

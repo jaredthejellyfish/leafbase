e7fed386f1883d74b520fd96c480a2f7dfd8854c
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-26 02:51:21 +0100

    Add Plus icon and review count to CommentSection component

--- components/CommentSection/index.tsx
+++ components/CommentSection/index.tsx
@@ -1,9 +1,11 @@
 import { ErrorBoundary } from 'react-error-boundary';
+import { Plus } from 'lucide-react';
 import React from 'react';
 
 import CommentCard from './CommentCard';
 
 type Props = {
+  strainName: string;
   comments: {
     comment: string;
     created_at: string;
@@ -17,12 +19,24 @@ type Props = {
   }[];
 };
 
-function CommentSection({ comments }: Props) {
+function CommentSection({ comments, strainName }: Props) {
   return (
-    <ErrorBoundary fallback={<div>Error!</div>}>
-      {comments.map((comment) => (
-        <CommentCard comment={comment} key={comment.id} />
-      ))}
+    <ErrorBoundary fallback={<div></div>}>
+      <>
+        <div className="my-3 flex flex-row items-center justify-between">
+          <div className="flex flex-row items-center gap-x-3 text-[1.6em] font-bold">
+            <span>Reviews for {strainName}</span>
+            <span className="text-2xl text-zinc-400">({comments?.length})</span>
+          </div>
+          <button className="mt-0.5 rounded border border-zinc-400 p-0.5">
+            <Plus className="h-6 w-6" />
+          </button>
+        </div>
+
+        {comments.map((comment) => (
+          <CommentCard comment={comment} key={comment.id} />
+        ))}
+      </>
     </ErrorBoundary>
   );
 }

32127cb21c60a025b18b3811df6d7f9c4d87f141
Gerard Hernandez <ger.almenara@gmail.com>
2023-11-27 19:20:44 +0100

    Update user profile with form data validation and error handling

--- lib/actions/user/updateUser.ts
+++ lib/actions/user/updateUser.ts
@@ -3,10 +3,28 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
+import { z } from 'zod';
 
+import type { UserMetadataExtended } from '@/lib/database/database_types';
 import type { Database } from '@/lib/database';
 
-export async function updateUser(formData: FormData) {
+// add a custom message to about that says: "This is a custom message"
+
+const UserProfileSchema = z.object({
+  about: z.string().max(500, { message: "About section must not exceed 500 characters." }),
+  name: z.string().max(30, { message: "Name must not exceed 30 characters." }),
+  username: z.string().max(30, { message: "Username must not exceed 30 characters." }),
+  phone: z.string().max(30, { message: "Phone number must not exceed 30 characters." }),
+  location: z.string().max(30, { message: "Location must not exceed 30 characters." }),
+  language: z.string().max(30, { message: "Language must not exceed 30 characters." }),
+  birth_date: z.string(),
+});
+
+
+export async function updateUser(
+  state: { error: null | string },
+  formData: FormData
+) {
   const cookieStore = cookies();
   const supabase = createRouteHandlerClient<Database>({
     cookies: () => cookieStore,
@@ -14,50 +32,114 @@ export async function updateUser(formData: FormData) {
 
   const { data, error } = await supabase.auth.getSession();
   const session = data.session;
+  const currentMetadata = session?.user.user_metadata as UserMetadataExtended;
 
   if (!session || error) {
     return {
-      status: null,
       error: 'No session',
     };
   }
 
-  const name = formData.get('name') || session?.user.user_metadata.full_name;
-  const username =
-    formData.get('username') || session?.user.user_metadata.display_name;
+  // ensure that formData is not null
+  if (!formData) {
+    return {
+      error: 'No form data',
+    };
+  }
 
-  const phone = formData.get('phone') || session?.user.user_metadata.phone;
-  const location =
+  const name: string =
+    formData.get('name') || session?.user.user_metadata.full_name;
+
+  const username: string =
+    formData.get('username') || session?.user.user_metadata.username;
+
+  const phone: string =
+    formData.get('phone') || session?.user.user_metadata.phone;
+
+  const location: string =
     formData.get('location') || session?.user.user_metadata.location;
 
-  const aboutMe =
+  const about: string =
     formData.get('aboutMe') || session?.user.user_metadata.aboutMe;
 
-  const languages =
+  const language: string =
     formData.get('language') || session?.user.user_metadata.languages;
 
-  const birthDate =
+  const birth_date: string =
     formData.get('birthDate') || session?.user.user_metadata.birthDate;
 
-  const newUserMetadata = {
-    ...session.user.user_metadata,
-    aboutMe,
+  // validate form data and ensure it is different from current metadata
+  try {
+    const validatedData = UserProfileSchema.parse({
+      about,
+      name,
+      username,
+      phone,
+      location,
+      language,
+      birth_date,
+    });
+
+    if (
+      validatedData.about === currentMetadata.about &&
+      validatedData.name === currentMetadata.name &&
+      validatedData.username === currentMetadata.username &&
+      validatedData.phone === currentMetadata.phone &&
+      validatedData.location === currentMetadata.location &&
+      validatedData.language === currentMetadata.language &&
+      validatedData.birth_date === currentMetadata.birth_date
+    ) {
+      return {
+        error: 'No changes made',
+      };
+    }
+  } catch (error) {
+    const zodError = error as z.ZodError;
+    const issues = zodError.issues.map((issue) => issue.message);
+    return {
+      error: issues[0] || 'Could not validate form data.',
+    };
+  }
+
+  const newUserMetadata: UserMetadataExtended = {
+    ...currentMetadata,
+    about,
     name,
-    displayName: username,
+    username,
     phone,
     location,
-    languages,
-    birthDate,
+    language,
+    birth_date,
   };
 
-  const { data: newUser, error: updateError } = await supabase.auth.updateUser({
-    data: newUserMetadata,
-  });
+  const { data: userMetatdataUpdated, error: updateError } =
+    await supabase.auth.updateUser({
+      data: newUserMetadata,
+    });
 
-  if (!newUser || updateError) {
+  if (!userMetatdataUpdated || updateError) {
     return {
-      status: null,
       error: 'Could not update user.',
+    };
+  }
+
+  const { data: profileUpdated, error: profileError } = await supabase
+    .from('profiles')
+    .update({
+      about,
+      name,
+      username,
+      phone,
+      location,
+      language,
+      birth_date,
+    })
+    .eq('id', session.user.id)
+    .select();
+
+  if (!profileUpdated || profileError) {
+    return {
+      error: 'Could not update profile.',
     };
   }
 

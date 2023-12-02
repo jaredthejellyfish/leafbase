874194183ef4b23059812d2d8b265131b1c60984
Gerard Hernandez <ger.almenara@gmail.com>
2023-12-02 04:32:42 +0100

    Refactor TextAreaAuto component to include additional props

--- components/TextAreaAuto/index.tsx
+++ components/TextAreaAuto/index.tsx
@@ -3,18 +3,41 @@
 import TextareaAutosize from 'react-textarea-autosize';
 import React from 'react';
 
+import { cn } from '@/lib/utils';
+
 type Props = {
   placeholder: string;
+  className?: string;
+  name?: string;
+  id?: string;
+  setValue?: (value: string) => void;
+  value?: string;
 };
 
-function TextAreaAuto({ placeholder }: Props) {
+function TextAreaAuto({
+  placeholder,
+  className,
+  name,
+  id,
+  value,
+  setValue,
+}: Props) {
   return (
     <TextareaAutosize
-      className="w-full rounded border border-zinc-500 bg-transparent p-1"
-      name="aboutMe"
-      id="about-me-textarea"
+      className={cn(
+        'w-full rounded border border-zinc-500 bg-transparent p-1',
+        className
+      )}
+      name={name || 'aboutMe'}
+      id={id || 'about-me-textarea'}
       minRows={3}
       placeholder={placeholder}
+      value={value ? value : undefined}
+      onChange={(e) => {
+        e.preventDefault();
+        if (e.target.value.length > 500) return;
+        else if (setValue) setValue(e.target.value);
+      }}
     />
   );
 }

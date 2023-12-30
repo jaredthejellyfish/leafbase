bc43a0081fbe284db0853e0248245bacdc9546f4
Gerard Hernandez <ger.almenara@gmail.com>
2023-12-30 15:54:37 -0800

    Refactor middleware to handle unauthorized redirects

--- middleware.ts
+++ middleware.ts
@@ -2,36 +2,20 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-const AUTH_CALLBACK_PATH = '/auth/callback';
-const AUTH_SIGNIN_PATH = '/auth/signin';
-const HOME_PATH = '/home';
-const ROOT_PATH = '/';
-
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-  const { data } = await supabase.auth.getSession();
-  const currentUrl = req.nextUrl.pathname;
-
-  const code = req.nextUrl.searchParams.get('code');
-  if (code && currentUrl !== AUTH_CALLBACK_PATH) {
-    const callbackUrl = new URL(
-      `${AUTH_CALLBACK_PATH}?code=${code}&redirect=${currentUrl}`,
-      req.nextUrl,
-    );
-    return NextResponse.redirect(callbackUrl);
-  }
-
-  if (!data.session && ![AUTH_SIGNIN_PATH, ROOT_PATH].includes(currentUrl)) {
-    return NextResponse.redirect(new URL(AUTH_SIGNIN_PATH, req.nextUrl));
-  }
-
-  if (data.session && currentUrl === ROOT_PATH) {
-    return NextResponse.redirect(new URL(HOME_PATH, req.nextUrl));
-  }
-
-  return NextResponse.next();
-}
+const unauthorizedRedirects = [
+  {
+    matcher: '/profile',
+    redirect: '/auth/signin',
+  },
+  {
+    matcher: '/profile/(.*)',
+    redirect: '/auth/signin',
+  },
+  {
+    matcher: '/home',
+    redirect: '/auth/signin',
+  },
+];
 
 export const config = {
   matcher: [
@@ -43,3 +27,36 @@ export const config = {
     '/home',
   ],
 };
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const currentUrl = req.nextUrl.pathname;
+
+  const code = req.nextUrl.searchParams.get('code');
+  if (code && currentUrl !== '/auth/callback') {
+    const callbackUrl = new URL(
+      `/auth/callback?code=${code}&redirect=${currentUrl}`,
+      req.nextUrl,
+    );
+    return NextResponse.redirect(callbackUrl);
+  }
+
+  const authPathMatchers = unauthorizedRedirects.map((r) => r.matcher);
+
+  for (const matcher of authPathMatchers) {
+    if (new RegExp(`^${matcher}$`).test(currentUrl)) {
+      const redirect = unauthorizedRedirects.find((r) =>
+        new RegExp(`^${r.matcher}$`).test(matcher),
+      );
+      if (redirect && !session) {
+        return NextResponse.redirect(new URL(redirect.redirect, req.nextUrl));
+      }
+    }
+  }
+
+  return NextResponse.next();
+}

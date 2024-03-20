59e25cf9aa84174a758ca50df7dcbe8e78004bca
Gerard Hernandez <ger.almenara@gmail.com>
2024-03-20 15:09:33 -0700

    Remove unused imports and components, and update SVG dimensions

--- src/components/Budtender/message.tsx
+++ src/components/Budtender/message.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 import { cn } from '@/lib/utils/cn';
+import dynamic from 'next/dynamic';
+
+const StrainCards = dynamic(() => import('./strain-card'), {
+  ssr: false
+});
 
 function genDateString(dateString?: string) {
   const date = new Date(dateString ? Date.parse(dateString) : Date.now());
@@ -38,35 +41,7 @@ function preprocessMarkdown(content: string) {
   });
 }
 
-function StrainCards({
-  strains,
-}: {
-  strains: {
-    slug: string;
-    image: string;
-    name: string;
-  }[];
-}) {
-  if (!strains.length) return null;
-  return (
-    <div className="flex flex-row gap-x-3 flex-wrap max-w-[305px] sm:max-w-[600px] ">
-      {strains?.map(({ slug, image, name }) => (
-        <Link
-          className="xs:w-[80px] flex w-[85] flex-col items-center rounded-lg dark:border bg-zinc-100/20 shadow dark:border-transparent dark:bg-zinc-700/70 mt-2"
-          href={`/strains/${slug}`}
-          key={slug}
-        >
-          <span className="mx-2 mb-1 mt-2 size-[62px] rounded dark:border bg-zinc-300/50 p-0.5 shadow dark:border-zinc-700/80 dark:bg-zinc-600/30 sm:size-[68px]">
-            <Image alt={slug} width={66} height={66} src={image} />
-          </span>
-          <span className="mb-1.5 max-w-[64px] truncate py-1 text-xs">
-            {name}
-          </span>
-        </Link>
-      ))}
-    </div>
-  );
-}
+
 
 function Message({
   author,
---
+++ src/components/Budtender/strain-card.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+function StrainCards({
+  strains,
+}: {
+  strains: {
+    slug: string;
+    image: string;
+    name: string;
+  }[];
+}) {
+  if (!strains.length) return null;
+  return (
+    <div className="flex flex-row gap-x-3 flex-wrap max-w-[305px] sm:max-w-[600px] ">
+      {strains?.map(({ slug, image, name }) => (
+        <Link
+          className="xs:w-[80px] flex w-[85] flex-col items-center rounded-lg dark:border bg-zinc-100/20 shadow dark:border-transparent dark:bg-zinc-700/70 mt-2"
+          href={`/strains/${slug}`}
+          key={slug}
+        >
+          <span className="mx-2 mb-1 mt-2 size-[62px] rounded dark:border bg-zinc-300/50 p-0.5 shadow dark:border-zinc-700/80 dark:bg-zinc-600/30 sm:size-[68px]">
+            <Image alt={slug} width={66} height={66} src={image} />
+          </span>
+          <span className="mb-1.5 max-w-[64px] truncate py-1 text-xs">
+            {name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default StrainCards;

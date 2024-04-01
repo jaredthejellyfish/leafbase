65157018824f2f9e15b14bac00e3ae9319823016
Gerard Hernandez <ger.almenara@gmail.com>
2024-04-01 10:26:48 -0700

    Update package.json and import Google Analytics

--- src/env.js
+++ src/env.js
@@ -11,17 +11,17 @@ export const env = createEnv({
       .enum(['development', 'test', 'production'])
       .default('development'),
 
-    OPENAI_API_KEY: z.string(),
+    OPENAI_API_KEY: z.string().refine((val) => val.startsWith('sk-')),
 
     KV_REST_API_READ_ONLY_TOKEN: z.string(),
     KV_REST_API_TOKEN: z.string(),
-    KV_REST_API_URL: z.string(),
-    KV_URL: z.string(),
+    KV_REST_API_URL: z.string().url(),
+    KV_URL: z.string().refine((val) => val?.startsWith('redis://')),
 
     VERCEL_ENV: z.enum(['production', 'preview', 'development']),
     VERCEL_URL: z.string().optional(),
 
-    GROQ_API_KEY: z.string(),
+    GROQ_API_KEY: z.string().refine((val) => val.startsWith('gsk_')),
   },
 
   /**
@@ -31,8 +31,9 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_SUPABASE_URL: z.string(),
+    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+    NEXT_PUBLIC_GTM: z.string().refine((val) => val.startsWith('G-')),
   },
 
   /**
@@ -51,6 +52,7 @@ export const env = createEnv({
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_GTM: process.env.NEXT_PUBLIC_GTM,
 
     VERCEL_ENV: process.env.VERCEL_ENV,
     VERCEL_URL: process.env.VERCEL_URL,
